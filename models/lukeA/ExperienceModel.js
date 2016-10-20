30f5c1647e948919b086b40540d6c2461993d3df
Nikita Korhonen <nikita.korhonen@metropolia.fi>
2016-10-20 10:43:45 +0300

    model

---
+++ models/lukeA/ExperienceModel.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Cell on 20-Oct-15.
+ */
+
+
+var db=require('../../mongodb/lukeAdb');
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema(
+    {
+        id:{type: String, required: '{PATH} is required!'},
+        patternTitle: String,
+        reportGain: Number,
+        upvoteGain: Number,
+        downvoteGain: Number,
+        active: Boolean
+    }
+);
+
+var model = db.model('ExperiencePattern', schema);
+
+module.exports =  model;
