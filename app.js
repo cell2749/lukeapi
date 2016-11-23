f19e25e2267239de59c60ec8e8806c731f4fb06e
Nikita Korhonen <nikita.korhonen@metropolia.fi>
2016-11-23 20:24:23 +0200

    access control

--- app.js
+++ app.js
@@ -70,6 +70,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req,res,next){
   res.set('Access-Control-Allow-Origin','*');
+  res.set('Access-Control-Allow-Headers','*');
+  res.set('Access-Control-Allow-Methods','GET, POST');
   next();
 });
 app.use('/lukeA', lukeA);
