9271a489136b4db2c060c0f6388ba6dd14b86dc3
Nikita Korhonen <nikita.korhonen@metropolia.fi>
2016-11-28 17:20:02 +0200

    Distance function

--- routes/utility.js
+++ routes/utility.js
@@ -27,22 +27,23 @@ Utility.prototype.allowKey = function (key) {
     }
     return true;
 };
-Utility.prototype.getCrow = function (lat1, lon1, lat2, lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = lat2 - lat1;  // below
-    var dLon = lon2 - lon1;
-    var a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(lat1) * Math.cos(lat2) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2)
-        ;
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c; // Distance in km
-    return d;
-}
 Utility.prototype.deg2rad = function (deg) {
     return deg * Math.PI / 180;
 };
+Utility.prototype.getCrow = function (lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);
+    var dLon = this.deg2rad(lon2-lon1);
+    var a =
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+        ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+};
+
 /**
  * @apiDefine removeStatus
  * @apiSuccessExample Success-Response:
